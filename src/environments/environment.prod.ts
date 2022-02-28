cad1e5a5dcd717e37e243034f5f8e39c310c1ca0
ImagineThomas <schmidt.thomas@hotmail.com>
2022-02-28 14:23:39 +0100

    project set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'test-bd2b3',
+    appId: '1:493088421612:web:ad3957d49ab912170117c9',
+    storageBucket: 'test-bd2b3.appspot.com',
+    apiKey: 'AIzaSyCjP5F6VMd3hvBPEPlhrc5VhubVGFydSVU',
+    authDomain: 'test-bd2b3.firebaseapp.com',
+    messagingSenderId: '493088421612',
+    measurementId: 'G-3B0YKLC44Q',
+  },
   production: true
 };
