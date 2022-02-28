ed30b51d94d2f356c77b0477287411a0c9fe4f9a
ImagineThomas <schmidt.thomas@hotmail.com>
2022-02-28 14:00:25 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'whatsuppdrei',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
